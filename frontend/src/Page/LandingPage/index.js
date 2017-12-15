6a1a28f1c6775cb69ca0861df1030af89bf69784
Ly Bao Khanh <baokhanh7m@hotmail.com>
2017-12-15 17:01:14 +0700

    Add fixture for Landing Page & eslint bug fix
    - Add logo team-radio
    - Add fixture images
    - Add fixture data
    - Remove warning global require

--- frontend/src/Page/LandingPage/index.js
+++ frontend/src/Page/LandingPage/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import './style.css';
+import fixture from '../../Fixture/landing';
+
 const LandingPage = () => (
   <div>
-    <h1>Welcome to the LandingPage Website!</h1>
+    <h1>{fixture.name}</h1>
+    <img src={fixture.stations[0].avatar} alt="" width="200" height="auto" />
   </div>
 );
 
