a0a21a3f4def12b0e39116d1b3cd860bb1853bf9
Tri Pham <pvtri96@gmail.com>
2017-12-18 15:47:42 +0700

    Fix MuiTheme config

---
+++ frontend/src/Page/Station/Playlist/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PlayList extends Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+PlayList.propTypes = {};
+
+export default PlayList;
