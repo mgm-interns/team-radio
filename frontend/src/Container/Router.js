c83966b4b6c56095aa9b7797fa1522c0e4f90538
Ly Bao Khanh <baokhanh7m@hotmail.com>
2017-12-18 13:11:22 +0700

    Add landing page layout

--- frontend/src/Container/Router.js
+++ frontend/src/Container/Router.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import LandingPage from '../Page/LandingPage/';
+import Landing from '../Page/Landing/';
 import Users from '../Page/Users';
 
 export default () => (
   <Switch>
-    <Route exact path="/" component={LandingPage} />
+    <Route exact path="/" component={Landing} />
     <Route exact path="/users" component={Users} />
   </Switch>
 );
--- frontend/src/Theme/Images.js
+++ frontend/src/Theme/Images.js
@@ -1,4 +1,5 @@
 const images = {
+  logo: require('../Images/logo/logo.png'),
   fixture: {
     avatar1: require('../Images/station-avatar/1.jpg'),
     avatar2: require('../Images/station-avatar/2.jpg'),
@@ -6,6 +7,7 @@ const images = {
     avatar4: require('../Images/station-avatar/4.jpg'),
     avatar5: require('../Images/station-avatar/5.jpg'),
   },
+  drummer: require('../Images/drummer.png'),
 };
 
 export default images;
