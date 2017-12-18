6397dfc673c3301a5ab9d3a53fd2d669652e87ef
Dung Le <dungle1811@gmail.com>
2017-12-18 15:15:46 +0700

    [HOTFIX] Add eslint rule

--- frontend/src/Page/Station/AddLink/index.js
+++ frontend/src/Page/Station/AddLink/index.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styles from './stypes.css';
+import { Input } from 'material-ui';
+import './stypes.css';
 
 class AddLink extends Component {
   render() {
     return (
-      <div>
+      <div className="add-link-container">
         <h3>ADD TO STATION 1</h3>
-        <div>
-          <div>
-            <input type="text" placeholder="Add your link..." />
+        <div className="add-link-box">
+          <div className="add-link-box-left">
+            <Input
+              type="text"
+              placeholder="Add your link..."
+              autoFocus
+              disableUnderline
+            />
           </div>
         </div>
       </div>
