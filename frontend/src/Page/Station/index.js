b3bf68fc5e4e161cdf5672a79576f817eac3ed3b
Dung Le <dungle1811@gmail.com>
2017-12-18 19:14:09 +0700

    Implement AddLink component

--- frontend/src/Page/Station/index.js
+++ frontend/src/Page/Station/index.js
@@ -4,18 +4,26 @@ import Grid from 'material-ui/Grid';
 import AddLink from './AddLink';
 import Playlist from './Playlist';
 
+const VIDEO_DEFAULT = {
+  name: '3 Hours of Christmas Music - Holiday Classics with the best new stuff',
+  thumbnail:
+    'https://upload.wikimedia.org/wikipedia/en/c/cb/Meghan_Trainor_Title_EP_Album_Cover.png',
+};
+
 class StationPage extends Component {
   render() {
     return (
-      <div>
-        <h1>Station Page</h1>
-        {/*
-          <AddLink />
-        */}
-        <Grid xs={4}>
+      <Grid direction="row" container style={{ margin: 0, width: '100%' }}>
+        <Grid item xs={6} md={7} lg={8}>
+          <h1>Station Page</h1>
+        </Grid>
+        <Grid item xs={6} md={5} lg={4}>
           <Playlist />
         </Grid>
-      </div>
+        <Grid item xs={12}>
+          <AddLink video_detail={VIDEO_DEFAULT} />
+        </Grid>
+      </Grid>
     );
   }
 }
