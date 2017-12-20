07dd213bac6a11a1ebef8549d9d3cbcb2a299480
KhacBac <ngokhacbac18695@gmail.com>
2017-12-20 16:49:24 +0700

    rename router.

--- backend/src/Routes/StationRoute.js
+++ backend/src/Routes/StationRoute.js
@@ -1,19 +1,20 @@
+/* eslint-disable */
 var station = require('../Controllers/StationCtroller');
 
 export default router => {
   // create a station
 
-  router.post('/add/addstation', function(req, res) {
+  router.post('/stations', function(req, res) {
     //   res.send('Ok');
     station.addStation(req, res);
   });
 
   //// get a station by name
-  router.get('/:stationName', function(req, res) {
+  router.get('/stations/:stationName', function(req, res) {
     station.getStationByName(req, res);
   });
   //// get list station but can limit if need
-  router.get('/list/stations', function(req, res) {
+  router.get('/stations', function(req, res) {
     station.getStations(req, res);
   });
 };
