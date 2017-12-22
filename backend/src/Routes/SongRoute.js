593f1d1e01a7f9ea91b350aa43e3d1fa055d07e9
Thang Nguyen <chienthangplus@gmail.com>
2017-12-22 11:58:31 +0700

    Change addNewSong to addSong, get api to post song api

--- backend/src/Routes/SongRoute.js
+++ backend/src/Routes/SongRoute.js
@@ -1,16 +1,24 @@
 // Prefix: api
 const SongController = require('../Controllers/SongController.js');
+const bodyParser = require('body-parser');
 
 async function getSongInformationApi(req, res) {
-  const songUrl = decodeURI(req.params['songUrl']);
-  const song = await SongController.addNewSong(songUrl);
-  console.log(song);
-  res.send(song);
+  if (req.body.url === undefined) {
+    req.send('none');
+  } else {
+    const songUrl = req.body.url;
+    const song = await SongController.addSong(songUrl);
+    if (song === null) {
+      res.send('none');
+    } else {
+      res.send(song);
+    }
+  }
 }
 
 export default router => {
   // For demonstration
-  router.get('/song/:songUrl', (req, res) => {
+  router.post('/song', (req, res) => {
     getSongInformationApi(req, res);
   });
 };
