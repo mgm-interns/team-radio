3cdaab1a8fdd60c3770ec5af79133114203b4d09
Thang Nguyen <chienthangplus@gmail.com>
2017-12-26 11:16:11 +0700

    Continue working with NowPlayingControler

--- backend/src/Controllers/NowPlayingController.js
+++ backend/src/Controllers/NowPlayingController.js
@@ -1,5 +1,18 @@
+import { TIMEOUT } from 'dns';
+
+const stationModel = require('../Models/Station');
+
 module.exports.start = function() {
   // Get information about nowplaying song in stations
+  stationModel.getStations(function(err, stationList) {
+    for (var i = 0; i < stationList.length; i++) {
+      if (stationList[i].now_playing != null) {
+        setTimeout(function() {
+          getNextNowPlayingSong(stationList[i].id);
+        }, stationList[i].now_playing.duration);
+      }
+    }
+  });
   return true;
 };
 
@@ -7,3 +20,7 @@ module.exports.addNowPlaying = function(stationNsp, songId, duration) {
   // Add new song to the station
   return true;
 };
+
+function getNextNowPlayingSong() {
+  return true;
+}
