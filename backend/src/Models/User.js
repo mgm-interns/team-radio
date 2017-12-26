bfb73e2b2b3b6b65021a81f710789cece4c4baf9
Superman <bao.nguyen.dangit@gmail.com>
2017-12-26 09:49:37 +0700

    A token was returned when signup success

--- backend/src/Models/User.js
+++ backend/src/Models/User.js
@@ -1,42 +1,43 @@
-var mongoose	= require('mongoose');
-var bcrypt		= require('bcrypt-nodejs');
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 //define the schema for our user model
 
 var userSchema = mongoose.Schema({
-	local: {
-		email	: String,
-		password: String
-	},
-	facebook:{
-		id		:String,
-		token	:String,
-		name	:String,
-		email	:String
-	},
-	twitter:{
-		id		:String,
-		token	:String,
-		displayName: String,
-		username:String
-	},
-	google:{
-		id 		:String,
-		token	:String,
-		email	:String,
-		name 	:String
-	}
+  local: {
+    email: String,
+    password: String,
+    username: String,
+  },
+  facebook: {
+    id: String,
+    token: String,
+    name: String,
+    email: String,
+  },
+  twitter: {
+    id: String,
+    token: String,
+    displayName: String,
+    username: String,
+  },
+  google: {
+    id: String,
+    token: String,
+    email: String,
+    name: String,
+  },
 });
 
 //methods ======================
 //generation a hash
-userSchema.methods.generateHash = function(password){
-	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 //checking if password is valid
-userSchema.methods.validPassword = function(password){
-	return bcrypt.compareSync(password, this.local.password);
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
 };
 
 //create the model for users and expose it ti our app
