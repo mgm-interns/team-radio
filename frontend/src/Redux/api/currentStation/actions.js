93bb67a18c69aaf1fa92247fdf913f4ba0267df1
Dung Le <dungle1811@gmail.com>
2017-12-28 17:56:08 +0700

    Add station id default

--- frontend/src/Redux/api/currentStation/actions.js
+++ frontend/src/Redux/api/currentStation/actions.js
@@ -6,28 +6,30 @@ import {
   CLIENT_LEAVE_STATION,
 } from 'Redux/actions';
 
+const DEFAULT_STATION_ID = '5a43122f01a6072810aadcb4';
+
 // Client action creator
-export const joinStation = stationId => ({
+export const joinStation = (stationId = DEFAULT_STATION_ID) => ({
   type: CLIENT_JOIN_STATION,
   payload: { userId: 0, stationId },
 });
 
-export const leaveStation = stationId => ({
+export const leaveStation = (stationId = DEFAULT_STATION_ID) => ({
   type: CLIENT_LEAVE_STATION,
   payload: { userId: 0, stationId },
 });
 
 export const addSong = ({ stationId, songUrl }) => ({
   type: CLIENT_ADD_SONG,
-  payload: { userId: 0, stationId, songUrl },
+  payload: { userId: 0, stationId: stationId || DEFAULT_STATION_ID, songUrl },
 });
 
 export const upVoteSong = ({ stationId, songId }) => ({
   type: CLIENT_UPVOTE_SONG,
-  payload: { useId: 0, stationId, songId },
+  payload: { useId: 0, stationId: stationId || DEFAULT_STATION_ID, songId },
 });
 
 export const downVoteSong = ({ stationId, songId }) => ({
   type: CLIENT_DOWNVOTE_SONG,
-  payload: { useId: 0, stationId, songId },
+  payload: { useId: 0, stationId: stationId || DEFAULT_STATION_ID, songId },
 });
