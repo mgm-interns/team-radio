e8f317456b7e656079ef03c13dbeb2bbd4107f4d
nguyentatnhac <nguyentatnhac@gmail.com>
2017-12-28 14:13:25 +0700

    create fixture data and api

--- backend/src/socket/events/createStation.js
+++ backend/src/socket/events/createStation.js
@@ -1,10 +1,11 @@
 import * as stationController from '../../controllers/station';
 import * as EVENTS from '../../const/actions';
+import * as controller from '../../../fixture/station';
 
 export default async (emitter, userId, stationName) => {
-  let station;
+  const station = await controller.addStation(userId, stationName);
   try {
-    station = await stationController.addStation(stationName, userId);
+    // station = await stationController.addStation(stationName, userId);
     emitter.emit(EVENTS.SERVER_CREATE_STATION_SUCCESS, {
       station: station,
     });
@@ -17,7 +18,8 @@ export default async (emitter, userId, stationName) => {
 
   if (station) {
     try {
-      const stations = await stationController.getAllAvailableStations();
+      // const stations = await stationController.getAllAvailableStations();
+      const stations = controller.stations;
       emitter.emitAll(EVENTS.SERVER_UPDATE_STATIONS, {
         stations: stations,
       });
--- backend/src/socket/events/joinStation.js
+++ backend/src/socket/events/joinStation.js
@@ -1,12 +1,9 @@
-import * as stationController from '../../controllers/station';
-import * as userController from '../../controllers/user';
 import * as EVENTS from '../../const/actions';
-import * as players from '../../players';
+import * as controller from '../../../fixture/station';
 
 export default async (emitter, userId, stationId) => {
-  let station;
+  const station = controller.stations.find(st => st.id === stationId);
   try {
-    station = await stationController.getStation(stationId);
     emitter.emit(EVENTS.SERVER_JOINED_STATION_SUCCESS, {
       station: station,
     });
@@ -19,16 +16,15 @@ export default async (emitter, userId, stationId) => {
   }
 
   if (station) {
-    const player = await players.getPlayer(stationId);
-    const nowPlaying = await player.getNowPlaying();
+    const nowPlaying = await controller.getNowplaying(stationId);
     emitter.emit(EVENTS.SERVER_UPDATE_NOW_PLAYING, {
       nowPlaying: nowPlaying,
+      // nowPlaying { index, songId, playingTime }
     });
 
     try {
-      const user = await userController.getUser(userId);
       emitter.emitToStation(stationId, EVENTS.SERVER_NEW_USER_JOINED, {
-        user: user,
+        user: userId,
       });
     } catch (err) {
       console.error(err);
