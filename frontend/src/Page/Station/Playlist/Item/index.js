8d2836701840939795cb7e4a90da0060ec7a3d13
Tri Pham <pvtri96@gmail.com>
2017-12-28 17:43:59 +0700

    Fix frontend actions & store

--- frontend/src/Page/Station/Playlist/Item/index.js
+++ frontend/src/Page/Station/Playlist/Item/index.js
@@ -5,7 +5,7 @@ import IconButton from 'material-ui/IconButton';
 import withStyles from 'material-ui/styles/withStyles';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { upVoteVideo, unUpVoteVideo } from 'Redux/api/currentStation/actions';
+import { upVoteSong } from 'Redux/api/currentStation/actions';
 import styles from './styles';
 
 /* eslint-disable no-return-assign */
@@ -22,7 +22,7 @@ class PlaylistItem extends Component {
       isUpvoted,
       playing,
       classes,
-      upVoteVideo,
+      upVoteSong,
       id,
     } = this.props;
     return (
@@ -42,7 +42,7 @@ class PlaylistItem extends Component {
         </Grid>
         <Grid item xs={2} className={classes.actions}>
           <IconButton
-            onClick={() => upVoteVideo(id)}
+            onClick={() => upVoteSong(id)}
             className={classes.action}
             color={isUpvoted ? 'primary' : 'default'}
           >
@@ -72,11 +72,11 @@ PlaylistItem.propTypes = {
   playing: PropTypes.bool,
   theme: PropTypes.any,
   classes: PropTypes.any,
-  upVoteVideo: PropTypes.func,
+  upVoteSong: PropTypes.func,
 };
 
 const mapDispatchToProps = dispatch => ({
-  upVoteVideo: videoId => dispatch(upVoteVideo({ videoId })),
+  upVoteSong: videoId => dispatch(upVoteSong({ videoId })),
 });
 
 export default compose(
--- frontend/src/Redux/api/currentStation/actions.js
+++ frontend/src/Redux/api/currentStation/actions.js
@@ -1,13 +1,10 @@
-// server action types
-export const SERVER_JOINED_STATION = 'SERVER:JOINED_STATION';
-export const SERVER_UPDATE_STATION = 'SERVER:UPDATE_STATION';
-
-// Server action creator
-
-// Client action types
-export const CLIENT_JOIN_STATION = 'CLIENT:JOIN_STATION';
-export const CLIENT_UPVOTE_VIDEO = 'CLIENT:UPVOTE_VIDEO';
-export const CLIENT_UN_UPVOTE_VIDEO = 'CLIENT:UN_UPVOTE_VIDEO';
+import {
+  CLIENT_UPVOTE_SONG,
+  CLIENT_JOIN_STATION,
+  CLIENT_DOWNVOTE_SONG,
+  SERVER_JOINED_STATION_SUCCESS,
+  SERVER_JOINED_STATION_FAILURE,
+} from 'Redux/actions';
 
 // Client action creator
 export const joinStation = station_name => ({
@@ -15,20 +12,12 @@ export const joinStation = station_name => ({
   payload: { station_name },
 });
 
-export const upVoteVideo = ({ stationId, videoId }) => ({
-  type: CLIENT_UPVOTE_VIDEO,
+export const upVoteSong = ({ stationId, videoId }) => ({
+  type: CLIENT_UPVOTE_SONG,
   payload: { stationId, videoId },
 });
 
-export const unUpVoteVideo = ({ stationId, videoId }) => ({
-  type: CLIENT_UN_UPVOTE_VIDEO,
+export const downVoteSong = ({ stationId, videoId }) => ({
+  type: CLIENT_DOWNVOTE_SONG,
   payload: { stationId, videoId },
-});
-
-// Friday demo action
-export const SET_CURRENT_STATION = 'SET_CURRENT_STATION';
-
-export const setCurrentStation = station => ({
-  type: SET_CURRENT_STATION,
-  payload: { station },
 });
