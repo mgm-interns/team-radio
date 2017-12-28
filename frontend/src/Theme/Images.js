216e9e866b82909a58f8f9a2a5f2c239266472b2
Tri Pham <pvtri96@gmail.com>
2017-12-28 12:02:35 +0700

    Remove drink-2.jpg

--- frontend/src/Theme/Images.js
+++ frontend/src/Theme/Images.js
@@ -1,6 +1,6 @@
 const images = {
   logo: require('../Images/logo/logo.png'),
-  drink: require('../Images/drink-2.jpg'),
+  drink: require('../Images/drink.jpg'),
   fixture: {
     avatar1: require('../Images/station-avatar/1.jpg'),
     avatar2: require('../Images/station-avatar/2.jpg'),
