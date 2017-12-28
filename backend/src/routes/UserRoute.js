ee734dd92e2c5a2b577d3a2eb858db723c23db35
Superman <bao.nguyen.dangit@gmail.com>
2017-12-28 13:01:25 +0700

    update user schema and refactor code

--- backend/src/routes/UserRoute.js
+++ backend/src/routes/UserRoute.js
@@ -1,37 +1,28 @@
-var express = require('express');
-var app = express();
-var apiRoutes = express.Router();
-var User = require('../models/User');
-var jwt = require('jsonwebtoken');
-var AuthController = require('../controllers/AuthController');
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+import authController from '../controllers/auth';
 
 export default router => {
-  router.post('/signup', function(req, res, done) {
-    User.findOne({ 'local.email': req.body.email }, function(err, user) {
-      //if there are any error
-      if (err) return done(err);
-
-      //if username is already exist
+  router.post('/signup', async (req, res) => {
+    try {
+      const user = await User.findOne({ email: req.body.email });
       if (user) {
         res.json({ message: 'Email is not available' });
       } else {
-        console.log('EMAIL : ');
-        console.log(req.body.email);
-
-        var newUser = new User();
-        newUser.local.email = req.body.email;
-        newUser.local.username = req.body.username;
-        newUser.local.password = newUser.generateHash(req.body.password);
+        const newUser = new User();
+        newUser.email = req.body.email;
+        newUser.name = req.body.name;
+        newUser.password = newUser.generateHash(req.body.password);
 
         newUser.save(function(err) {
           if (err) throw err;
           const payload = {
-            email: newUser.local.email,
-            username: newUser.local.username,
+            email: newUser.email,
+            name: newUser.name,
           };
 
-          var token = jwt.sign(payload, req.app.get('superSecret'), {
-            expiresIn: 1440 * 7, //expires in 24 hours *****************************
+          const token = jwt.sign(payload, req.app.get('superSecret'), {
+            expiresIn: 1440 * 7, // expires in 24 hours
           });
           res.json({
             message: 'signup success',
@@ -39,36 +30,36 @@ export default router => {
           });
         });
       }
-    });
+    } catch (err) {
+      throw err;
+    }
   });
 
-  router.post('/login', function(req, res) {
-    User.findOne({ 'local.email': req.body.email }, function(err, user) {
-      if (err) throw err;
-
+  router.post('/login', async (req, res) => {
+    try {
+      const user = await User.findOne({ email: req.body.email });
       if (!user) {
         res.json({
           success: false,
           message: 'Authenticate failed. User not Found.',
         });
       } else if (user) {
-        //check if password correct
         if (!user.validPassword(req.body.password)) {
           res.json({
-            success: false,
             message: 'Authenticate failed. Wrong password.',
+            success: false,
           });
         } else {
-          //if user is found and password is right
-          //create a token with only our given payload
-          //we don't want to pass in the entrie user since that has the password
+          // if user is found and password is right
+          // create a token with only our given payload
+          // we don't want to pass in the entrie user since that has the password=
           const payload = {
-            email: user.local.email,
-            username: user.local.username,
+            email: user.email,
+            name: user.name,
           };
 
-          var token = jwt.sign(payload, req.app.get('superSecret'), {
-            expiresIn: 1440 * 7, //expires in 24 hours *****************************
+          const token = jwt.sign(payload, req.app.get('superSecret'), {
+            expiresIn: 1440, // expires in 24 hours *****************************
           });
 
           res.json({
@@ -78,21 +69,21 @@ export default router => {
           });
         }
       }
-    });
+    } catch (err) {
+      throw err;
+    }
   });
 
-  router.use(function(req, res, next) {
-    console.log('MIDDLEWARE IS INVOKED');
-    AuthController(req, res, next);
-    next();
+  router.use((req, res, next) => {
+    authController(req, res, next);
   });
 
-  //test function *************************************
-  router.get('/', function(req, res, next) {
+  // test function *************************************
+  router.get('/', (req, res) => {
     res.json({ message: 'Welcome to the coolest API on earth!' });
   });
 
-  router.get('/User', function(req, res) {
+  router.get('/User', (req, res) => {
     User.find({}, function(err, users) {
       res.json(users);
     });
