70af291e05d96598f91c899cbfaec51574060709
Ly Bao Khanh <baokhanh7m@hotmail.com>
2017-12-29 12:20:21 +0700

    [HOTFIX] Fix error

--- frontend/src/Page/Landing/index.js
+++ frontend/src/Page/Landing/index.js
@@ -21,27 +21,7 @@ class Landing extends Component {
       const { station_name } = nextProps.currentStation.station;
       history.push(`/station/${station_name}`);
     }
-
-  // async componentDidMount() {
-  //   const { notification: { app, browser } } = this.props;
-  //   let count = 0;
-  //   const duration = 2000;
-  //   // // Call notification 3 times
-  //   const interval = setInterval(async () => {
-  //     await app.success({
-  //       message: 'Authentication is fucking failed!',
-  //       duration,
-  //     });
-  //     await browser.info({
-  //       message: 'Authentication is fucking failed!',
-  //       duration,
-  //     });
-  //     count += 1;
-  //     if (count > 3) {
-  //       clearInterval(interval);
-  //     }
-  //   }, 300);
-  // }
+  }
 
   render() {
     return [
@@ -64,6 +44,4 @@ const mapStateToProps = state => ({
   currentStation: state.api.currentStation,
 });
 
-export default compose(connect(mapStateToProps), withRouter)(
-  Landing,
-);
+export default compose(connect(mapStateToProps), withRouter)(Landing);
