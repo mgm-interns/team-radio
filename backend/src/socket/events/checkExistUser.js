2435c39a45d3c7856e2df205e53436bbdd763ff9
nguyentatnhac <nguyentatnhac@gmail.com>
2018-01-03 14:31:02 +0700

    Add action check exists email

--- backend/src/socket/events/checkExistUser.js
+++ backend/src/socket/events/checkExistUser.js
@@ -2,9 +2,9 @@ import * as userController from '../../controllers/user';
 import * as EVENTS from '../../const/actions';
 
 export default async (emitter, email) => {
-  const isExist = await userController.isExistUserHandler(email);
+  const isExistEmail = await userController.isExistUserHandler(email);
 
-  emitter.emit(EVENTS.SERVER_UPDATE_PLAYLIST, {
-    isExist: isExist,
+  emitter.emit(EVENTS.SERVER_CHECK_EMAIL_RESPONSE, {
+    status: isExistEmail,
   });
 };
