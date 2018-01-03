6dc61f1fb11e2d11b5de51ed556aaa3964ccc322
Dung Le <dungle1811@gmail.com>
2018-01-03 16:57:32 +0700

    Add stage changed

--- frontend/src/Redux/api/stations/actions.js
+++ frontend/src/Redux/api/stations/actions.js
@@ -1,6 +1,6 @@
 import { CLIENT_CREATE_STATION } from 'Redux/actions';
 
-export const createStation = ({ userId = 0, stationName }) => ({
+export const createStation = ({ userId = '0', stationName }) => ({
   type: CLIENT_CREATE_STATION,
   payload: { userId, stationName },
 });
