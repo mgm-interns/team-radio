aa4e98ba773eaf1638dd585347927ce2971d0a03
Nhac Nguyen Tat <nguyentatnhac@gmail.com>
2018-01-08 11:38:47 +0700

    Refactor code Prepare status code

--- backend/src/controllers/station.js
+++ backend/src/controllers/station.js
@@ -71,6 +71,7 @@ export const addSong = async (stationId, songUrl, userId = null) => {
   } catch (err) {
     throw err;
   }
+
   if (!station) {
     throw new Error(`Station id ${stationId} is not exist!`);
   }
--- backend/src/socket/events/leaveStation.js
+++ backend/src/socket/events/leaveStation.js
@@ -3,12 +3,12 @@ import * as EVENTS from '../../const/actions';
 
 export default async (emitter, userId, stationId, socket) => {
   try {
-    const user = await userController.getUserById(userId);
+    const { userName } = await userController.getUserById(userId);
     socket.leaveAll();
     emitter.emitToStation(stationId, EVENTS.SERVER_USER_LEFT, {
-      user: user,
+      user: userName,
     });
   } catch (err) {
-    console.log(err);
+    console.log('Error leave station: ' + err);
   }
 };
