1187d4f2697416645ee608775cabed70e4ebffda
Ly Bao Khanh <baokhanh7m@hotmail.com>
2018-01-25 11:39:59 +0700

    Update user score rules

--- frontend/src/Page/Help/index.js
+++ frontend/src/Page/Help/index.js
@@ -1,8 +1,5 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 import { NavBar, Footer } from 'Component';
-import Grid from 'material-ui/Grid';
-import Typography from 'material-ui/Typography';
-import List, { ListItem } from 'material-ui/List';
 import { compose } from 'redux';
 import { withStyles } from 'material-ui/styles';
 
@@ -11,35 +8,24 @@ import Markdown from 'react-markdown';
 import styles from './styles';
 import scoreSource from './score';
 
-class Help extends Component {
-  constructor(props) {
-    super(props);
+const defaultCover =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYUZbD4KRNnATuFo0ZqOOmAU6ecExSjSLxxzDlnTrHgNQs6bvl';
 
-    this.handleControlsChange = this.handleControlsChange.bind(this);
-    this.handleMarkdownChange = this.handleMarkdownChange.bind(this);
-    this.state = {
-      markdownSrc: scoreSource.content,
-    };
-  }
-
-  handleMarkdownChange(evt) {
-    this.setState({ markdownSrc: evt.target.value });
-  }
-
-  handleControlsChange(mode) {
-    this.setState({ htmlMode: mode });
-  }
-
-  render() {
-    const { classes } = this.props;
-
-    return [
-      <NavBar key={1} color="primary" />,
-      <div key={2} className={classes.wrapper}>
-        <Markdown source={this.state.markdownSrc} />
-      </div>,
-    ];
-  }
-}
+const Help = ({ classes }) => (
+  <Fragment>
+    <NavBar key={1} color="primary" />
+    <div key={2} className={classes.container}>
+      <div className={classes.coverContainer}>
+        <div className={classes.coverWrapper}>
+          <img src={defaultCover} className={classes.backgroundImg} />
+        </div>
+      </div>
+      <div className={classes.wrapper}>
+        <Markdown source={scoreSource.content} />
+      </div>
+    </div>
+    <Footer key={3} />
+  </Fragment>
+);
 
 export default compose(withStyles(styles))(Help);
