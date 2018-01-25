da9230170b1a736c28c34b94f92f42b851dce7b5
Nhac Nguyen Tat <nguyentatnhac@gmail.com>
2018-01-25 16:20:09 +0700

    Improve search feature

--- backend/src/controllers/search.js
+++ backend/src/controllers/search.js
@@ -2,15 +2,32 @@ let Station = null;
 
 export default (query, limit) =>
   new Promise(resolve => {
-    const regex = new RegExp('^' + query);
-    // Station.find({ $text: { $search: query } }) // Full text search
+    /**
+     * Full text search using text indexes: Best performance
+     * Problem: Cannot search part of string like `like` in SQL DB
+     * i.e: Cannot use `kha`, `anh` to search something like `Khanh`
+     */
+    // Station.find({ $text: { $search: query } })
+    //   .select({ station_name: 1, station_id: 1 }) // What fields will be select
+    //   .limit(limit)
+    //   .exec((err, result) => {
+    //     if (err) return resolve([]);
+    //     return resolve(result);
+    //   });
+
+    /**
+     * Search without text indexes: Bad performance
+     * Problem: Bad performance, this query runs too slow
+     */
     Station.find({
       $or: [
-        { $text: { $search: query } },
-        { station_name: { $regex: regex } },
-        { station_id: { $regex: regex } },
+        /**
+         * Define search rule: Search by station_name or station_id
+         */
+        { station_name: { $regex: new RegExp(query, 'i') } },
+        { station_id: { $regex: new RegExp(query, 'i') } },
       ],
-    }) // Full text search
+    })
       .select({ station_name: 1, station_id: 1 }) // What fields will be select
       .limit(limit)
       .exec((err, result) => {
