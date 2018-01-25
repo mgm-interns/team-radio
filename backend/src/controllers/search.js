5feb9b774e8f7722ce69e6847ade8bb7d98e77d8
Nhac Nguyen Tat <nguyentatnhac@gmail.com>
2018-01-25 13:27:45 +0700

    HOT FIX: Search syntax

--- backend/src/controllers/search.js
+++ backend/src/controllers/search.js
@@ -2,8 +2,15 @@ let Station = null;
 
 export default (query, limit) =>
   new Promise(resolve => {
-    // const regex = new RegExp('/' + query + '/');
-    Station.find({ $text: { $search: query } }) // Full text search
+    const regex = new RegExp('^' + query);
+    // Station.find({ $text: { $search: query } }) // Full text search
+    Station.find({
+      $or: [
+        { $text: { $search: query } },
+        { station_name: { $regex: regex } },
+        { station_id: { $regex: regex } },
+      ],
+    }) // Full text search
       .select({ station_name: 1, station_id: 1 }) // What fields will be select
       .limit(limit)
       .exec((err, result) => {
