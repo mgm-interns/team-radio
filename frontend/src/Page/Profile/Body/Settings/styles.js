73e62f34efd4533f2d978d7b3416d3dc4128b3cc
Nam <namhoang.td09@gmail.com>
2018-01-29 13:54:49 +0700

    Add form scroll

--- frontend/src/Page/Profile/Body/Settings/styles.js
+++ frontend/src/Page/Profile/Body/Settings/styles.js
@@ -50,4 +50,15 @@ export default ({ spacing }) => ({
   menuItem: {
     cursor: 'pointer',
   },
+  modal: {
+    position: 'absolute',
+    width: '35vw',
+    top: `50%`,
+    left: `50%`,
+    transform: `translate(-50%, -50%)`,
+    border: '1px solid #e5e5e5',
+    backgroundColor: '#fff',
+    boxShadow: '0 5px 15px rgba(0, 0, 0, .5)',
+    padding: 32,
+  },
 });
