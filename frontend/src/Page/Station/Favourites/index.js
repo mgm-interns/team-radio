5e11f24bc0bd6a867f0a0a580e4df9ecd8552a55
Dung Le <dungle1811@gmail.com>
2018-01-31 17:34:03 +0700

    Fix virtualized list of favourite

--- frontend/src/Page/Station/Favourites/index.js
+++ frontend/src/Page/Station/Favourites/index.js
@@ -1,52 +1,70 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
-import reverse from 'lodash/reverse';
-import sortBy from 'lodash/sortBy';
-import VirtualList from 'react-virtual-list';
+import orderBy from 'lodash/orderBy';
+import List from 'react-virtualized/dist/commonjs/List';
 import Grid from 'material-ui/Grid';
 import { withStyles } from 'material-ui/styles';
 import Item from './Item';
 import styles from './styles';
 
 class FavouriteVideos extends Component {
-  _getFavouriteList({ virtual, itemHeight }) {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      listWidth: 0,
+      listHeight: 0,
+    };
+
+    this._renderRow = this._renderRow.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      listWidth: this.divElement.clientWidth,
+      listHeight: this.divElement.clientHeight,
+    });
+  }
+
+  _renderRow({ index, style, key }) {
+    const { data } = this.props;
+
+    const item = orderBy(data, ['created_date'], ['desc'])[index];
+
     return (
-      <ul style={{ paddingTop: 0, paddingBottom: 0 }}>
-        {virtual.items.map((video, index) => (
-          <li key={index}>
-            <Item
-              key={video.song_id || index}
-              {...video}
-              style={{ height: itemHeight }}
-            />
-          </li>
-        ))}
-      </ul>
+      <div key={key} style={style}>
+        <Item {...item} />
+      </div>
     );
   }
 
   render() {
     const { className, style, data } = this.props;
-    const options = {
-      initialState: {
-        firstItemIndex: 0, // show first 8 items
-        lastItemIndex: 7, // during initial render
-      },
-    };
-    const VirtualFavouriteList = VirtualList(options)(this._getFavouriteList);
+    const { listWidth, listHeight } = this.state;
 
     return (
       <Grid
         item
         xs={12}
         className={className}
-        style={{ ...style, overflowY: 'auto', overflowX: 'hidden' }}
+        style={{ ...style, overflowX: 'hidden' }}
       >
-        <VirtualFavouriteList
-          items={reverse(sortBy(data, (item, index) => index))}
-          itemHeight={80}
-        />
+        <div
+          style={{ height: '100%' }}
+          ref={element => {
+            this.divElement = element;
+          }}
+        >
+          <List
+            width={listWidth}
+            height={listHeight}
+            rowCount={data.length}
+            rowHeight={80}
+            rowRenderer={this._renderRow}
+            overscanRowCount={10}
+          />
+        </div>
       </Grid>
     );
   }
