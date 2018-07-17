cf5ef8b5b734908dc0cb2faed0829dbc8f68bdbd
Tri Pham <pvtri96@gmail.com>
2018-07-17 10:25:29 +0700

    Refactor station worker

--- backend/src/worker/stations.js
+++ backend/src/worker/stations.js
@@ -4,7 +4,6 @@ import { workerlog, workerError } from './logger';
 
 /* eslint-disable no-await-in-loop */
 export const scanStations = async () => {
-  const today = new Date().getTime();
   const stations = await Station.find(
     { is_delete: false },
     { station_name: 1, created_date: 1, playlist: 1, station_id: 1, _id: 0 },
@@ -12,41 +11,46 @@ export const scanStations = async () => {
   if (!stations) return null;
 
   const removedStations = [];
+  // Need to loop through every station one by one, not all
   // eslint-disable-next-line no-restricted-syntax
   for (const mStation of stations) {
-    const station = mStation.toObject();
-    if (station && station.playlist) {
-      let timestamp;
-      if (station.playlist.length === 0) {
-        timestamp = station.created_date + 7 * 24 * 60 * 60 * 1000;
-        //                                day  hour min  sec  msec
-      } else {
-        const latestSong = _.maxBy(station.playlist, song => song.created_date);
-        if (!latestSong) {
-          const result = await deleteStation(station.station_id);
-          removedStations.push(result);
-        }
-        timestamp = latestSong.created_date + 30 * 24 * 60 * 60 * 1000;
-        //                                    day  hour min  sec  msec
-      }
-      // If the station has no song and is created 7 or 30 days ago (base on above condition)
-      // Kill the station
-      if (timestamp < today) {
-        workerlog('station inactive for 7/30 days', station.station_id);
-        const result = await deleteStation(station.station_id);
-        removedStations.push(result);
-      }
-    } else {
-      // Delete the station if this station doesn't have a playlist
-      workerlog("station that doesn't have playlist", station.station_id);
-      const result = await deleteStation(station.station_id);
-      removedStations.push(result);
-    }
+    const result = await resolveStation(mStation.toObject());
+    removedStations.push(result);
   }
 
   // Remove all null result
   return removedStations.filter(id => id !== null);
 };
+
+async function resolveStation(station) {
+  if (!station.playlist) {
+    // Delete the station if this station doesn't have a playlist
+    workerlog("station that doesn't have playlist", station.station_id);
+    return deleteStation(station.station_id);
+  }
+
+  const today = new Date().getTime();
+  let timestamp;
+  if (station.playlist.length === 0) {
+    timestamp = station.created_date + 7 * 24 * 60 * 60 * 1000;
+    //                                day  hour min  sec  msec
+    workerlog('station inactive for 7 days', station.station_id);
+  } else {
+    const latestSong = _.maxBy(station.playlist, song => song.created_date);
+    if (!latestSong) {
+      return deleteStation(station.station_id);
+    }
+    timestamp = latestSong.created_date + 30 * 24 * 60 * 60 * 1000;
+    //                                    day  hour min  sec  msec
+    workerlog('station inactive for 30 days', station.station_id);
+  }
+  // If the station has no song and is created 7 or 30 days ago (base on above condition)
+  // Kill the station
+  if (timestamp < today) {
+    return deleteStation(station.station_id);
+  }
+  return null;
+}
 
 async function deleteStation(stationId) {
   workerlog('start removing station with id: ', stationId);
@@ -63,7 +67,7 @@ async function deleteStation(stationId) {
       .catch(() => {
         workerError('remove station with id: ' + stationId);
       });
-  workerError('The stations are less than 10, stop this action ' + stationId);
+  workerError('There are less than 10 stations, stop killing ' + stationId);
   return null;
 }
 
