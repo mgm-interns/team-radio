0674e1430a249f288ddd306f6820bded311024ab
Tri Pham <pvtri96@gmail.com>
2018-08-04 15:48:21 +0700

    Fix menu bar on native app and typo.

--- frontend/src/Component/MenuBar/styles.js
+++ frontend/src/Component/MenuBar/styles.js
@@ -1,19 +1,35 @@
-export default ({ palette }) => ({
+export default ({ palette, spacing }) => ({
   container: {
     height: 30,
     backgroundColor: palette.primary['800'],
-    '-webkit-app-region': 'drag',
     '-webkit-user-select': 'none',
+  },
+  draggable: {
+    width: '100%',
+    height: 36,
+    '-webkit-app-region': 'drag',
   },
   toolbar: {
     minHeight: 0,
     paddingRight: 0,
+    paddingLeft: 0,
+  },
+  contentWrapper: {
+    padding: '0 !important',
   },
   content: {
     margin: 0,
   },
+  contentContainer: {
+    margin: 0,
+    flexWrap: 'nowrap',
+  },
   rightContainer: {
-    marginLeft: 'auto',
+    width: 150,
+    paddingRight: spacing.doubleBaseMargin,
+    '& button': {
+      height: '100%',
+    },
   },
   actionButton: {
     color: palette.white,
